b85f0a6fa0b8dd7cc8acb7399a7fc40fa9e07e1b
Stephan <stephan@stzal.com>
2011-04-17 19:37:43 +0200

    added missing semicolons to richards.js

--- js/bench/v8/v1/richards.js
+++ js/bench/v8/v1/richards.js
@@ -30,7 +30,7 @@
 // benchmark from:
 //
 //    http://www.cl.cam.ac.uk/~mr10/Bench.html
-// 
+//
 // The benchmark was originally implemented in BCPL by
 // Martin Richards.
 
@@ -154,7 +154,7 @@ Scheduler.prototype.addHandlerTask = function (id, priority, queue) {
  * @param {Packet} queue the queue of work to be processed by the task
  */
 Scheduler.prototype.addDeviceTask = function (id, priority, queue) {
-  this.addTask(id, priority, queue, new DeviceTask(this))
+  this.addTask(id, priority, queue, new DeviceTask(this));
 };
 
 /**
@@ -384,7 +384,7 @@ IdleTask.prototype.run = function (packet) {
 };
 
 IdleTask.prototype.toString = function () {
-  return "IdleTask"
+  return "IdleTask";
 };
 
 /**
