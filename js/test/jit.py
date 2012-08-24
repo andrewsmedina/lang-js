50f6e69a080c4b5108dce474162b0ecf48fdb306
Stephan <stephan@stzal.com>
2012-08-24 10:44:56 +0200

    updated jit test

--- js/test/jit.py
+++ js/test/jit.py
@@ -8,27 +8,32 @@ conftest.option = o
 from pypy.jit.metainterp.test.support import LLJitMixin
 
 from js import interpreter
-from js.jscode import JsCode, jitdriver
+from js.jscode import jitdriver
 
 class TestLLtype(LLJitMixin):
     def test_append(self):
+        #code = """
+          #var x = {i:0};
+          #function f() {
+            #while(x.i < 100) {
+              #x = {i:x.i + 1};
+            #}
+          #}
+          #f();
+        #"""
         code = """
-          var x = {i:0};
-          function f() {
-            while(x.i < 100) {
-              x = {i:x.i + 1};
+            var i = 0;
+            while(i < 100) {
+                i += 1;
             }
-          }
-          f();
+            return i;
         """
         jsint = interpreter.Interpreter()
-        bytecode = JsCode()
-        interpreter.load_source(code, '').emit(bytecode)
-        func = bytecode.make_js_function()
 
-        def interp_w(c):
-            jitdriver.set_param("inlining", True)
-            code_val = func.run(jsint.global_context)
-        interp_w(1)
-        self.meta_interp(interp_w, [6], listcomp=True, backendopt=True, listops=True)
+        def interp_w():
+            #jitdriver.set_param("inlining", True)
+            code_val = jsint.run_src(code)
+            return code_val.ToNumber()
+        #assert interp_w() == 100
+        assert self.meta_interp(interp_w, [], listcomp=True, backendopt=True, listops=True) == 100
 
