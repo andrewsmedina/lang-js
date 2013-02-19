26c6fa1a43dfb866e7593dc95bce48de944fdd32
Stephan <stephan@stzal.com>
2013-02-19 13:02:29 +0100

    added translatable date and time functions

--- js/builtins/date.py
+++ js/builtins/date.py
@@ -1,9 +1,16 @@
 from rpython.rlib.rfloat import NAN, isnan
-from rpython.rlib.objectmodel import we_are_translated
 
-import datetime
 from js.builtins import get_arg
-from js.object_space import w_return, hide_on_translate, _w
+from js.object_space import w_return, _w
+from js import rtime
+
+fMSEC = 1
+fSEC = 2
+fMIN = 3
+fHOUR = 4
+fDAY = 1
+fMONTH = 2
+fYEAR = 3
 
 
 def setup(global_object):
@@ -103,14 +110,9 @@ def now(this, args):
 
 @w_return
 def to_string(this, args):
-    if not we_are_translated():
-        d = w_date_to_datetime(this)
-        local = to_local(d)
-
-        s = local.strftime('%a %b %d %Y %H:%M:%S GMT%z (%Z)')
-        return s
-
-    return this.PrimitiveValue().to_string()
+    t = make_jstime(this)
+    s = t.strftime('%a %b %d %Y %H:%M:%S GMT%z (%Z)', local=True)
+    return s
 
 
 # 15.9.5.8
@@ -127,146 +129,121 @@ def get_time(this, args):
 
 # 15.9.5.10
 @w_return
-@hide_on_translate(0)
 def get_full_year(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.year
+    t = make_jstime(this)
+    return t.year(local=True)
 
 
 # 15.9.5.11
 @w_return
-@hide_on_translate(0)
 def get_utc_full_year(this, args):
-    d = w_date_to_datetime(this)
-    return d.year
+    t = make_jstime(this)
+    return t.year()
 
 
 # 15.9.5.12
 @w_return
-@hide_on_translate(0)
 def get_month(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.month
+    t = make_jstime(this)
+    return t.month(local=True)
 
 
 # 15.9.5.13
 @w_return
-@hide_on_translate(0)
 def get_utc_month(this, args):
-    d = w_date_to_datetime(this)
-    return d.day
+    t = make_jstime(this)
+    return t.month()
 
 
 # 15.9.5.14
 @w_return
-@hide_on_translate(0)
 def get_date(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.day
+    t = make_jstime(this)
+    return t.day(local=True)
 
 
 # 15.9.5.15
 @w_return
-@hide_on_translate(0)
 def get_utc_date(this, args):
-    d = w_date_to_datetime(this)
-    return d.day
+    t = make_jstime(this)
+    return t.day()
 
 
 # 15.9.5.16
 @w_return
-@hide_on_translate(0)
 def get_day(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.weekday()
+    t = make_jstime(this)
+    return t.wday(local=True)
 
 
 # 15.9.5.17
 @w_return
-@hide_on_translate(0)
 def get_utc_day(this, args):
-    d = w_date_to_datetime(this)
-    return d.weekday()
+    t = make_jstime(this)
+    return t.wday()
 
 
 # 15.9.5.18
 @w_return
-@hide_on_translate(0)
 def get_hours(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.hour
+    t = make_jstime(this)
+    return t.hour(local=True)
 
 
 # 15.9.5.19
 @w_return
-@hide_on_translate(0)
 def get_utc_hours(this, args):
-    d = w_date_to_datetime(this)
-    return d.hour
+    t = make_jstime(this)
+    return t.hour()
 
 
 # 15.9.5.20
 @w_return
-@hide_on_translate(0)
 def get_minutes(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.minute
+    t = make_jstime(this)
+    return t.min(local=True)
 
 
 # 15.9.5.21
 @w_return
-@hide_on_translate(0)
 def get_utc_minutes(this, args):
-    d = w_date_to_datetime(this)
-    return d.minute
+    t = make_jstime(this)
+    return t.min()
 
 
 # 15.9.5.22
 @w_return
-@hide_on_translate(0)
 def get_seconds(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.second
+    t = make_jstime(this)
+    return t.sec(local=True)
 
 
 # 15.9.5.23
 @w_return
-@hide_on_translate(0)
 def get_utc_seconds(this, args):
-    d = w_date_to_datetime(this)
-    return d.second
+    t = make_jstime(this)
+    return t.sec()
 
 
 # 15.9.5.24
 @w_return
-@hide_on_translate(0)
 def get_milliseconds(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    return local.microsecond / 1000
+    t = make_jstime(this)
+    return t.msec(local=True)
 
 
 # 15.9.5.25
 @w_return
-@hide_on_translate(0)
 def get_utc_milliseconds(this, args):
-    d = w_date_to_datetime(this)
-    return d.microsecond / 1000
+    t = make_jstime(this)
+    return t.msec()
 
 
 # 15.9.5.26
 @w_return
-@hide_on_translate(0)
 def get_timezone_offset(this, args):
-    d = w_date_to_datetime(this)
-    offset = -1 * (d.utcoffset().total_seconds() / 60)
+    t = make_jstime(this)
+    offset = -1 * t.utc_offset() / 60
     return offset
 
 
@@ -290,154 +267,137 @@ def set_time(this, args):
 
 # 15.9.5.28
 @w_return
-@hide_on_translate(0)
 def set_milliseconds(this, args):
-    time_args = to_timeargs(args, 1)
-    return set_datetime(this, time_args)
+    time_args = to_timeargs(args, fMSEC)
+    return change_wdate(this, time_args, local=True)
 
 
 # 15.9.5.29
 @w_return
-@hide_on_translate(0)
 def set_utc_milliseconds(this, args):
-    time_args = to_timeargs(args, 1)
-    return set_utc_datetime(this, time_args)
+    time_args = to_timeargs(args, fMSEC)
+    return change_wdate(this, time_args)
 
 
 # 15.9.5.30
 @w_return
-@hide_on_translate(0)
 def set_seconds(this, args):
-    time_args = to_timeargs(args, 2)
-    return set_datetime(this, time_args)
+    time_args = to_timeargs(args, fSEC)
+    return change_wdate(this, time_args, local=True)
 
 
 # 15.9.5.30
 @w_return
-@hide_on_translate(0)
 def set_utc_seconds(this, args):
-    time_args = to_timeargs(args, 2)
-    return set_utc_datetime(this, time_args)
+    time_args = to_timeargs(args, fSEC)
+    return change_wdate(this, time_args)
 
 
 # 15.9.5.32
 @w_return
-@hide_on_translate(0)
 def set_minutes(this, args):
-    time_args = to_timeargs(args, 3)
-    return set_datetime(this, time_args)
+    time_args = to_timeargs(args, fMIN)
+    return change_wdate(this, time_args, local=True)
 
 
 # 15.9.5.33
 @w_return
-@hide_on_translate(0)
 def set_utc_minutes(this, args):
-    time_args = to_timeargs(args, 3)
-    return set_utc_datetime(this, time_args)
+    time_args = to_timeargs(args, fMIN)
+    return change_wdate(this, time_args)
 
 
 # 15.9.5.34
 @w_return
-@hide_on_translate(0)
 def set_hours(this, args):
-    time_args = to_timeargs(args, 4)
-    return set_datetime(this, time_args)
+    time_args = to_timeargs(args, fHOUR)
+    return change_wdate(this, time_args, local=True)
 
 
 # 15.9.5.35
 @w_return
-@hide_on_translate(0)
 def set_utc_hours(this, args):
-    time_args = to_timeargs(args, 4)
-    return set_utc_datetime(this, time_args)
+    time_args = to_timeargs(args, fHOUR)
+    return change_wdate(this, time_args)
 
 
 # 15.9.5.36
 @w_return
-@hide_on_translate(0)
 def set_date(this, args):
-    date_args = to_dateargs(args, 1)
-    return set_datetime(this, date_args)
+    date_args = to_dateargs(args, fDAY)
+    return change_wdate(this, date_args, local=True)
 
 
 # 15.9.5.37
 @w_return
-@hide_on_translate(0)
 def set_utc_date(this, args):
-    date_args = to_dateargs(args, 1)
-    return set_utc_datetime(this, date_args)
+    date_args = to_dateargs(args, fDAY)
+    return change_wdate(this, date_args)
 
 
 # 15.9.5.38
 @w_return
-@hide_on_translate(0)
 def set_month(this, args):
-    date_args = to_dateargs(args, 2)
-    return set_datetime(this, date_args)
+    date_args = to_dateargs(args, fMONTH)
+    return change_wdate(this, date_args, local=True)
 
 
 # 15.9.5.39
 @w_return
-@hide_on_translate(0)
 def set_utc_month(this, args):
-    date_args = to_dateargs(args, 2)
-    return set_utc_datetime(this, date_args)
+    date_args = to_dateargs(args, fMONTH)
+    return change_wdate(this, date_args)
 
 
 # 15.9.5.38
 @w_return
-@hide_on_translate(0)
 def set_full_year(this, args):
-    date_args = to_dateargs(args, 3)
-    return set_datetime(this, date_args)
+    date_args = to_dateargs(args, fYEAR)
+    return change_wdate(this, date_args, local=True)
 
 
 # 15.9.5.39
 @w_return
-@hide_on_translate(0)
 def set_utc_full_year(this, args):
-    date_args = to_dateargs(args, 3)
-    return set_utc_datetime(this, date_args)
+    date_args = to_dateargs(args, fYEAR)
+    return change_wdate(this, date_args)
 
 
 # B.2.4
 @w_return
-@hide_on_translate(0)
 def get_year(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    y = local.year - 1900
+    t = make_jstime(this)
+    y = t.year(local=True) - 1900
     return y
 
 
 # B.2.5
 @w_return
-@hide_on_translate(0)
 def set_year(this, args):
     arg0 = get_arg(args, 0)
     year = arg0.ToInteger()
 
     if isnan(year) or year < 0 or year > 99:
-        this.set_primitive_value(NAN)
+        this.set_primitive_value(_w(NAN))
         return NAN
 
     y = year + 1900
-
-    return set_datetime(this, [y])
+    c = JsDateChange()
+    c.set_year(y)
+    change_wdate(this, c)
+    return y
 
 
 # 15.9.5.42
 @w_return
-@hide_on_translate(0)
 def to_utc_string(this, args):
-    d = w_date_to_datetime(this)
-    s = d.strftime('%c %z')
+    t = make_jstime(this)
+    s = t.strftime('%c %z')
     return s
 
 
 # B.2.6
 @w_return
-@hide_on_translate(0)
 def to_gmt_string(this, args):
     return to_utc_string(this, args)
 
@@ -456,105 +416,247 @@ def utc(this, args):
 ####### helper
 
 
-def to_timeargs(args, count):
-    a = argv(args)
-    rfilled = rfill_args(a, count)
-    lfilled = lfill_args(rfilled, 7)
-    return lfilled
+def to_timeargs(args, fill):
+    a = w_argi(args)
+    c = JsDateChange()
+    ac = len(a)
+
+    if fill == fMSEC:
+        c.set_msecond(a[0])
+    elif fill == fSEC:
+        c.set_second(a[0])
+        if ac > 1:
+            c.set_msecond(a[1])
+    elif fill == fMIN:
+        c.set_minute(a[0])
+        if ac > 1:
+            c.set_second(a[1])
+        if ac > 2:
+            c.set_msecond(a[2])
+    elif fill == fHOUR:
+        c.set_hour(a[0])
+        if ac > 1:
+            c.set_minute(a[1])
+        if ac > 2:
+            c.set_second(a[2])
+        if ac > 3:
+            c.set_msecond(a[3])
+
+    return c
 
 
-def to_dateargs(args, count):
-    a = argv(args)
-    rfilled = rfill_args(a, count)
-    lfilled = lfill_args(rfilled, 3)
-    return lfilled
+def to_dateargs(args, fill):
+    a = w_argi(args)
+    c = JsDateChange()
+    ac = len(a)
+
+    if fill == fDAY:
+        c.set_day(a[0])
+    elif fill == fMONTH:
+        c.set_month(a[0])
+        if ac > 1:
+            c.set_day(a[1])
+    elif fill == fYEAR:
+        c.year = a[0]
+        if ac > 1:
+            c.set_month(a[1])
+        if ac > 2:
+            c.set_day(a[2])
+
+    return c
 
 
-def set_datetime(this, args):
-    d = w_date_to_datetime(this)
-    local = to_local(d)
-    new_d = change_datetime(local, *args)
+class JsDateChange(object):
+    year = 0
+    has_year = False
+    month = 0
+    has_month = False
+    day = 0
+    has_day = False
+    hour = 0
+    has_hour = False
+    minute = 0
+    has_minute = False
+    second = 0
+    has_second = False
+    msecond = 0
+    has_msecond = False
 
-    u = datetime_to_msecs(new_d)
-    this.set_primitive_value(u)
+    def set_year(self, year):
+        self.has_year = True
+        self.year = year
 
-    return u
+    def set_month(self, month):
+        self.has_month = True
+        self.month = month
 
+    def set_day(self, day):
+        self.has_day = True
+        self.day = day
 
-def set_utc_datetime(this, args):
-    d = w_date_to_datetime(this)
-    new_d = change_datetime(d, *args)
+    def set_hour(self, hour):
+        self.has_hour = True
+        self.hour = hour
 
-    u = datetime_to_msecs(new_d)
-    this.set_primitive_value(u)
+    def set_minute(self, minute):
+        self.has_minute = True
+        self.minute = minute
 
-    return u
+    def set_second(self, second):
+        self.has_second = True
+        self.second = second
 
-
-def argv(args):
-    return [arg.ToInteger() for arg in args]
-
-
-def lfill_args(args, count):
-    if count > 0:
-        missing = count - len(args)
-        return ([None] * missing) + args
-    return args
-
-
-def rfill_args(args, count):
-    if count > 0:
-        missing = count - len(args)
-        return args + ([None] * missing)
-
-    return args
-
-
-def change_datetime(d, year=None, month=None, day=None, hour=None, minute=None, second=None, ms=None):
-    args = {}
-    if year is not None:
-        args['year'] = year
-    if month is not None:
-        args['month'] = month
-    if day is not None:
-        args['day'] = day
-    if hour is not None:
-        args['hour'] = hour
-    if minute is not None:
-        args['minute'] = minute
-    if second is not None:
-        args['second'] = second
-    if ms is not None:
-        mu_sec = ms * 1000
-        args['microsecond'] = mu_sec
-    return d.replace(**args)
+    def set_msecond(self, msecond):
+        self.has_msecond = True
+        self.msecond = msecond
 
 
-def w_date_to_datetime(w_date):
-    msecs = w_date.PrimitiveValue().ToInteger()
-    return msecs_to_datetime(msecs)
+def change_wdate(w_date, time_change, local=False):
+    t = make_jstime(w_date)
+
+    if time_change.has_year:
+        t.set_year(time_change.year, local)
+
+    if time_change.has_month:
+        t.set_month(time_change.month, local)
+
+    if time_change.has_day:
+        t.set_day(time_change.day, local)
+
+    if time_change.has_hour:
+        t.set_hour(time_change.hour, local)
+
+    if time_change.has_minute:
+        t.set_min(time_change.minute, local)
+
+    if time_change.has_second:
+        t.set_sec(time_change.second, local)
+
+    if time_change.has_msecond:
+        t.set_msec(time_change.msecond, local)
+
+    w_t = _w(t.to_msec_epoc())
+    w_date.set_primitive_value(w_t)
+
+    return w_t
 
 
-def msecs_to_datetime(timestamp_msecs):
-    from dateutil.tz import tzutc
-
-    seconds_since_epoch = timestamp_msecs / 1000
-    msecs = timestamp_msecs - seconds_since_epoch * 1000
-    timestamp = seconds_since_epoch + (msecs / 1000.0)
-    utc = datetime.datetime.utcfromtimestamp(timestamp)
-    utc = utc.replace(tzinfo=tzutc())
-
-    return utc
+def w_argi(args):
+    argi = []
+    for arg in args:
+        a = arg.ToInteger()
+        assert isinstance(a, int)
+        argi += [a]
+    return argi
 
 
-def datetime_to_msecs(d):
-    from time import mktime
-    timestamp = int(mktime(d.utctimetuple()))
-    msecs = timestamp * 1000
-    msecs += (d.microsecond / 1000)
-    return msecs
+def _change_tuple(t, idx, value):
+    assert len(t) == 9
+    l = list(t)
+    l[idx] = value
+    return (l[0], l[1], l[2], l[3], l[4], l[5], l[6], l[7], l[8])
 
 
-def to_local(datetime):
-    from dateutil.tz import tzlocal
-    return datetime.astimezone(tzlocal())
+def _get_tuple_at(t, idx):
+    l = list(t)
+    return l[idx]
+
+
+class JsTime(object):
+    def __init__(self, msec_timestamp):
+        self._timep = msec_timestamp / 1000
+        self._msec = msec_timestamp - self._timep * 1000
+
+    def __str__(self):
+        return 'JsTime(%d)' % (self.to_msec_epoc(), )
+
+    def _get_time(self, local):
+        if local:
+            return self.localtime()
+        return self.gmtime()
+
+    def gmtime(self):
+        return rtime.gmtime(self._timep)
+
+    def localtime(self):
+        return rtime.localtime(self._timep)
+
+    def _set_time(self, tm, local):
+        if local:
+            self._timep = int(rtime.mktime(tm))
+        else:
+            self._timep = int(rtime.timegm(tm))
+
+    def _update_time(self, component, value, local):
+        tm = self._get_time(local)
+        new_tm = _change_tuple(tm, component, value)
+        self._set_time(new_tm, local)
+
+    def _get_time_component(self, component, local):
+        return _get_tuple_at(self._get_time(local), component)
+
+    def _get_timestamp(self):
+        return self._timep
+
+    def to_msec_epoc(self):
+        return (self._get_timestamp() * 1000) + (self.msec())
+
+    def year(self, local=False):
+        return self._get_time_component(rtime.tmYEAR, local)
+
+    def set_year(self, year, local=False):
+        self._update_time(rtime.tmYEAR, year, local)
+
+    def month(self, local=False):
+        return self._get_time_component(rtime.tmMONTH, local)
+
+    def set_month(self, month, local=False):
+        self._update_time(rtime.tmMONTH, month, local)
+
+    def day(self, local=False):
+        return self._get_time_component(rtime.tmDAY, local)
+
+    def set_day(self, day, local=False):
+        self._update_time(rtime.tmDAY, day, local)
+
+    def hour(self, local=False):
+        return self._get_time_component(rtime.tmHOUR, local)
+
+    def set_hour(self, hour, local=False):
+        self._update_time(rtime.tmHOUR, hour, local)
+
+    def min(self, local=False):
+        return self._get_time_component(rtime.tmMIN, local)
+
+    def set_min(self, min, local=False):
+        self._update_time(rtime.tmMIN, min, local)
+
+    def sec(self, local=False):
+        return self._get_time_component(rtime.tmSEC, local)
+
+    def set_sec(self, sec, local=False):
+        self._update_time(rtime.tmSEC, sec, local)
+
+    def msec(self, local=False):
+        return self._msec
+
+    def set_msec(self, msec, local=False):
+        assert msec < 1000
+        self._msec = msec
+
+    def wday(self, local=False):
+        return self._get_time_component(rtime.tmWDAY, local)
+
+    def strftime(self, format, local=False):
+        tm = self._get_time(local)
+        s = rtime.strftime(format, tm)
+        return s
+
+    def utc_offset(self):
+        o = rtime.mktime(self.localtime()) - rtime.mktime(self.gmtime())
+        return o
+
+
+def make_jstime(w_obj):
+    msecs = w_obj.PrimitiveValue().ToInteger()
+    return JsTime(msecs)
