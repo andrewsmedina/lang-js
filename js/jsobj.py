79bfc6386ecaa0f6649c80d1284afd032f049185
Stephan <stephan@stzal.com>
2013-03-21 16:35:41 +0100

    backed out of 9e9af1fdf61d due to performance issues

--- js/jsobj.py
+++ js/jsobj.py
@@ -1187,6 +1187,7 @@ MASK_16 = (2 ** 16) - 1
 
 
 @enforceargs(int)
+@jit.elidable
 def int32(n):
     if n & (1 << (32 - 1)):
         res = n | ~MASK_32
@@ -1197,11 +1198,13 @@ def int32(n):
 
 
 @enforceargs(int)
+@jit.elidable
 def uint32(n):
     return n & MASK_32
 
 
 @enforceargs(int)
+@jit.elidable
 def uint16(n):
     return n & MASK_16
 
